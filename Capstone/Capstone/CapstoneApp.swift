8df1dea8b1cd1700d4b4c16439c554e99023b151
Kelvin Lau <kelvin.kl.lau@gmail.com>
2025-03-08 14:58:54 -0800

    Adds fresh xcode project

---
+++ Capstone/Capstone/CapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneApp.swift
+//  Capstone
+//
+//  Created by Kelvin Lau on 2025-03-08.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
