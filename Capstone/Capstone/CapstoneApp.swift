27a604f242758d4d58f547bb9b7a6e0cc1dcb072
Kelvin Lau <kelvin.kl.lau@gmail.com>
2025-03-08 14:59:55 -0800

    Fixes indentation

--- Capstone/Capstone/CapstoneApp.swift
+++ Capstone/Capstone/CapstoneApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct CapstoneApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
